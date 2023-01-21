d1ac3051b5a73b3abc86ca26c2cb7bae65ea4292
Kerim Cisneros de La Llave <kerim_2000@live.com.mx>
2023-01-21 14:09:16 -0700

    Se borro para validar
    Para que no pida el login cada vez que le movemos

--- src/modules/authentication/views/LoginView.vue
+++ src/modules/authentication/views/LoginView.vue
@@ -1,60 +1,21 @@
 <template>
-    <div class=" centrado">
-      <form class="form-horizontal">
-        <h2>Iniciar sesión</h2>
-        <div class="form-group">
-          <label for="email" class="control-label col-xs-2"
-            >Correo Electronico</label
-          >
-          <div class="col-xs-10">
-            <input
-              type="text"
-              class="form-control"
-              id="email"
-              v-model="email"
-              required
-            />
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="password" class="control-label col-xs-2">Contraseña</label>
-          <div class="col-xs-10">
-            <input
-              type="password"
-              class="form-control"
-              id="password"
-              v-model="password"
-              required
-            />
-          </div>
-        </div>
-        <br />
-        <div class="form-group">
-          <div class="col-xs-offset-2 col-xs-10">
+   
             <button type="submit" class="btn button" @click="goToMoney">
               Iniciar sesión
             </button>
                | 
             <router-link to="/registro" class="router-link">No tienes cuenta?</router-link>
-          </div>
-        </div>
-      </form>
-    </div>
+          
   </template>
     
     <script>
   export default {
-    data() {
-      return {
-        email: "",
-        password: "",
-      };
-    },
+    
     methods: {
       goToMoney() {
-        if (this.email && this.password) {
+       
           return this.$router.push({ name: "inicio" });
-        }
+        
   
         
       },
