30496eda4b5f3361a5acbafaadb3f5568031cbdd
Kerim Cisneros de La Llave <kerim_2000@live.com.mx>
2023-01-22 12:18:09 -0700

    Juan puto

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <template>
 
   <div class="app">
-    <SideBar/>
     <router-view/>
   </div>
 </template>
-
-
-<script setup>
-import SideBar from "./modules/money_app/components/SideBar.vue"
-</script>
 
 
 <style >
@@ -21,7 +15,6 @@ import SideBar from "./modules/money_app/components/SideBar.vue"
 *{
   margin: 0;
   padding: 0;
-  box-sizing: border-box;
   font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
 }
 
@@ -29,14 +22,7 @@ body{
   background: var(--MainBgColor) ;
 
 }
-#app{
-  margin: 20px;
-}
-.app {
-  display: flex;
-  border-radius:25px;
-  background-color: #d7d7d7;
 
-}
+
 </style>
 
