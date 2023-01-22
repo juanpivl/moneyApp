2d7a2444688a0d29912842df3c842be698ef2689
Juan Vargas <62524570+juanpivl@users.noreply.github.com>
2023-01-22 12:13:00 -0600

    kerim pendejo

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
 
   <div class="app">
-    <SideBar/>
     <router-view/>
   </div>
 </template>
 
 
-<script setup>
-import SideBar from "./modules/money_app/components/SideBar.vue"
+<script >
 </script>
 
 
@@ -21,7 +19,6 @@ import SideBar from "./modules/money_app/components/SideBar.vue"
 *{
   margin: 0;
   padding: 0;
-  box-sizing: border-box;
   font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
 }
 
@@ -29,14 +26,7 @@ body{
   background: var(--MainBgColor) ;
 
 }
-#app{
-  margin: 20px;
-}
-.app {
-  display: flex;
-  border-radius:25px;
-  background-color: #d7d7d7;
 
-}
+
 </style>
 
